9e1adc80792b6ae78c324ac3deeb0bed836d7d79
DX-0041\RayJi <rayji@5gg.win>
2024-09-26 14:31:21 +0800

    npm i unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,6 +21,32 @@ export default defineConfig({
       },
     }),
     vueJsx(),
+    AutoImport({
+      // 在哪些檔案裡可以直接引入
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      // 需要自動匯入的插件，自訂匯入的API
+      imports: [
+        "vue",
+        "vue-i18n",
+        "vue-router",
+        "pinia",
+        {
+          "@/dayjsSetup": [["default", "dayjs"]],
+          lodash: [["default", "_"]],
+        },
+      ],
+      dts: "./src/types/auto-imports.d.ts",
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      dts: "./src/types/components.d.ts",
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
